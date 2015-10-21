5061ae8bb5597740c22e804211b7b128d84b80e0
kuwabara <vol.kuwabara@1pac.jp>
2015-10-21 19:12:51 +0900

    master側

--- frontend_src/grunt/compass.js
+++ frontend_src/grunt/compass.js
@@ -11,7 +11,7 @@ module.exports = {
   css_sample: {
     options: {
       sassDir: './scss/_sample',
-      cssDir: '../public/_dev/sample/css',
+      cssDir: '../sample/css',
       outputStyle: 'expanded',
       noLineComments: true
     }
