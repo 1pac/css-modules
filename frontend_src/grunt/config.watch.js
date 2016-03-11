601149fe3c72d420e3603f6d3da03193bac62b7f
kuwabara <vol.kuwabara@1pac.jp>
2016-03-11 17:33:00 +0900

    サンプルページがChromeでクラッシュする問題を改善

--- frontend_src/grunt/config.watch.js
+++ frontend_src/grunt/config.watch.js
@@ -2,15 +2,26 @@ module.exports = {
   // watch ---------------------------------------------------
   // CSS
   watch : {
-    css_shared: {
+
+    // lib ------------------------------------------------
+    css_lib: {
       files: [
         './scss/lib/*.scss',
-        './scss/lib/*/*.scss',
+        './scss/lib/*/*.scss'
+      ],
+      tasks: ['css_shared', 'css_sample']
+    },
+
+    // shared ------------------------------------------------
+    css_shared: {
+      files: [
         './scss/shared/*.scss',
         './scss/shared/*/*.scss'
       ],
       tasks: ['css_shared']
     },
+
+    // sample ------------------------------------------------
     css_sample: {
       files: [
         './scss/_sample/*.scss'
